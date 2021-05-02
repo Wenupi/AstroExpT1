d9c08b2d01ff23060b8d7d760331df4d23139ea0
Wenupi <71337338+Wenupi@users.noreply.github.com>
2021-05-01 20:55:55 -0400

    Update P3
    Se añade el fiteo de las temperaturas máximas

--- Tareas/Tarea1.py
+++ Tareas/Tarea1.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from scipy.optimize import curve_fit
 import glob
-from scipy.integrate import trapz
+from scipy.integrate import trapz, simps
 from matplotlib.font_manager import FontProperties
 
 df_HCT = pd.read_excel('/Users/javier/Documents/University/5th semester/Experimental astronomy/'
@@ -29,7 +29,6 @@ T_cold = df_HCT.iloc[3, 4]
 Y = dBm_to_Watts(float(df_HCT.iloc[2, 1]))/dBm_to_Watts(float(df_HCT.iloc[3, 1]))
 
 T_rec_exp = T_rec(T_hot, T_cold, Y)
-print('El valor de $T_{REC}$ es:', T_rec_exp)
 
 
 """
@@ -85,6 +84,7 @@ fig2, ax2 = plt.subplots(5, 3, figsize=(15,20))
 # temperatura máxima y coordenadas galácticas
 
 T_maxs=[]
+T_integrada = []
 
 # Para colocar los gráficos en la figura 2 se definen las position_i que indica
 # la columna en la que se colocará para su fila correspondiente
@@ -159,9 +159,65 @@ for i in range(11, 26):
     else:  # por si algo sale mal
         None
 
+    T_integrada_i = trapz(T)
+    T_integrada.append(('sdf_1'+str(i), T_integrada_i, lii, bii))
+
 # Se guarda el plot final con todos los subplots
 ax2[0,1].set_title('Espectro de Orión', fontsize=30, fontproperties=font)
-ax2[2,0].set_ylabel('Temperatura', fontsize=25, fontproperties=font)
-ax2[4,1].set_xlabel('Velocidad', fontsize=25, fontproperties=font)
+ax2[2,0].set_ylabel('Temperatura [K]', fontsize=25, fontproperties=font)
+ax2[4,1].set_xlabel(r'Velocidad $\frac{km}{s}$', fontsize=25, fontproperties=font)
 fig2.tight_layout()    
 fig2.savefig("Espectros")
+
+#print('La temperatura integrada es:', T_integrada)
+print('Las temperaturas máximas son:', T_maxs)
+T_max_208 = np.zeros((3,2))
+T_max_208[0, 0]=-19.510527
+T_max_208[1, 0]=-19.385527
+T_max_208[2, 0]=-19.260527
+
+for i in range(0, 15):
+    if T_maxs[i][2]==208.996002:
+        if T_maxs[i][3]==-19.510527:
+            T_max_208[0, 1]+=T_maxs[i][1]
+        elif T_maxs[i][3]==-19.385527:
+            T_max_208[1, 1]+=T_maxs[i][1]
+        elif T_maxs[i][3]==-19.260527:
+            T_max_208[2, 1]+=T_maxs[i][1]
+
+T_max_19 = np.zeros((3,2))
+T_max_19[0, 0]=208.863495
+T_max_19[1, 0]=208.996002
+T_max_19[2, 0]=209.12851
+
+
+for i in range(0, 15):
+    if T_maxs[i][3]==-19.385527:
+        if T_maxs[i][2]==208.863495:
+            T_max_19[0, 1]+=T_maxs[i][1]
+        elif T_maxs[i][2]==208.996002:
+            T_max_19[1, 1]+=T_maxs[i][1]
+        elif T_maxs[i][2]==209.12851:
+            T_max_19[2, 1]+=T_maxs[i][1]
+
+#print('Temperaturas maximas promedio', T_max_208[:,1]/3)
+#print('Temperaturas maximas promedio', T_max_19[:,1]/3)
+#print(T_max_208)
+
+fg_208 = [np.max(T_max_208[:,1]/3), -19, 1]  #first guess
+coefs_208, cov_208 = curve_fit(f_gauss,T_max_208[:,0], T_max_208[:,1]/3, p0=fg_208)
+t0_208, M_208, S_208 = coefs_208[0],coefs_208[1],coefs_208[2]
+
+fg_19 = [np.max(T_max_19[:,1]/3), 208.99, 1]  #first guess
+coefs_19, cov_19 = curve_fit(f_gauss,T_max_19[:,0], T_max_19[:,1]/3, p0=fg_19)
+t0_19, M_19, S_19 = coefs_19[0],coefs_19[1],coefs_19[2]
+
+fig3, ax3 = plt.subplots(2, 1, figsize=(3.5, 6.5))
+ax3[0].scatter(T_max_208[:,0], T_max_208[:,1]/3, color='black', label='lii=208')
+ax3[0].plot(np.linspace(-19.510527, -19.260527, 20), f_gauss(np.linspace(-19.510527, -19.260527, 20), t0_208, M_208, S_208), color='#4a9923')
+ax3[1].scatter(T_max_19[:,0], T_max_19[:,1]/3, color='black', label='bii=-19')
+ax3[1].plot(np.linspace(208.863495, 209.12851, 20), f_gauss(np.linspace(208.863495, 209.12851, 20), t0_19, M_19, S_19), color='#4a9923')
+ax3[0].legend()
+ax3[1].legend()
+fig3.tight_layout()    
+fig3.savefig("Fit_temperaturas_maxs")
